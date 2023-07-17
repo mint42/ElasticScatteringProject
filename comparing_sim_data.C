c40d423ee6108d18f626bc8038eb6c59e6afec16
Ari <arireedy42@gmail.com>
2023-07-17 09:29:13 -0400

    creating more functionality

--- comparing_sim_data.C
+++ comparing_sim_data.C
@@ -1,13 +1,13 @@
 // This program draws the pre and post cut histograms on the same plot with
 // different colors
 
+#include <string>
 #include "TFile.h"
+#include "TTree.h"
 #include "TH1D.h"
 #include "TH1.h"
 #include "TCanvas.h"
 #include "TMath.h"
-#include "TTreeReader.h"
-#include "TTreeReaderValue.h"
 
 #define SYM_FILE "../sim/worksim/test.root"
 
@@ -20,43 +20,60 @@
 // #define EXP_FILE "hms_files/Pion_hms_replay_production_13854_-1.root"
 // #define EXP_FILE "hms_files/Pion_hms_replay_production_13857_-1.root"
 
+#define NUM_VARS 2
+static const char	**sim_var_names = [ "hsxfp", "hsyfp" ];
+static const char	**exp_var_names = [	"H.dc.x.fp", "H.dc.y.fp" ];
+
+//										"hsxpfp",
+//										"hsypfp",
+//										"nu",
+//										"Q2",
+//										"W",
+//										"epsilon"];
+//										"hsdelta",
+//										"hsyptar",
+//										"hsxptar",
+//										"hsytar"
+
+static THStack	*load_comparison_hist(TTree *exp_tree, TTree *sim_tree, char *hist_name, String exp_var, String sim_var)
+{
+	// draw var with "graph off". " >> hist" writes to a histogram in the gDir
+	// middle string is for cuts. needs ">> hist" too if a cut is present
+	exp_tree->Draw((exp_var + " >> hist").c_str(), "", "goff");
+	TH1D	*exp_hist = (TH1D*)gDirectory->Get("hist"); // grab hist
+
+	sim_tree->Draw((sim_var + " >> hist2").c_str(), "", "goff");
+	TH1D	*sim_hist = (TH1D*)gDirectory->Get("hist2");
+
+	// set color
+	exp_hist->SetLineColor(kBlue);
+	sim_hist->SetLineColor(kRed);
+
+	// class to plot multiple histograms together THStack(name, title)
+	THStack	*hs = new THStack("hists", hist_name);
+
+	// add hists to the stack and plot together
+	hs->Add(exp_hist);
+	hs->Add(sim_hist);
+	return (hs);
+}
+
 void 			comparing_sim_data()
 {
 	// loads the root files
 	TFile		*exp_data = TFile::Open(EXP_FILE);
-	TFile		*sym_data = TFile::Open(SYM_FILE);
+	TFile		*sim_data = TFile::Open(SIM_FILE);
 
-	// load tree from files
+	// load trees from files
 	TTree		*exp_tree;
 	exp_data->GetObject("T", exp_tree);
 
-	TTree		*sym_tree;
-	sym_data->GetObject("h10", sym_tree);
+	TTree		*sim_tree;
+	sim_data->GetObject("h10", sim_tree);
 
-	// "draw" desired variable and save that drawing to the "hist" histogram
-	// stored in the gDirectory. 
-	// middle string is for cuts. needs ">> hist" too if a cut is present
-	// goff will keep it from actually drawing
-	exp_tree->Draw("H.dc.x_fp >> hist", "", "goff");
-	// grab the histogram from the gDir
-	TH1D	*H_dc_x_fp_hist = (TH1D*)gDirectory->Get("hist");
+	
+	// load_*(hist name, exp variable's name, sim variable's name)
+	THStack		*xfp_hist = load_comparison_hist(exp_data, "hist name", exp_var_names[0], sim_var_names[0]);
 
-	exp_tree->Draw("hsxfp >> hist2", "", "goff");
-	TH1D	*hsxfp_hist = (TH1D*)gDirectory->Get("hist2");
-
-//	// color and print
-//	H_dc_x_fp_hist->SetLineColor(kRed);
-//	H_dc_x_fp_hist->Draw();
-
-	// set color
-	H_dc_x_fp_hist->SetLineColor(kRed);
-	hsxfp_hist->SetLineColor(kBlue);
-
-	// class to plot multiple histograms together THStack(name, title)
-	THStack	*hs = new THStack("hists", "Run 12080 yptar comparison");
-
-	// add hists to the stack and plot together
-	hs->Add(H_dc_x_fp_hist);
-	hs->Add(hsxfp_hist);
-	hs->Draw("NOSTACK"); // don't stack them
+	xfp_hist->Draw("NOSTACK"); // don't stack them
 }
